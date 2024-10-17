60221eda76480a2af8800ff69a5afaeaa74ec5cc
RustgaarAhmad <rustgaarahmad@gmail.com>
2024-10-17 10:39:36 +0500

    Add files via upload

---
+++ task9cpa.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+string check_point(int h,int x,int y)
+{
+    // Border
+    if ((y == 0) || (y==h  &&  (x<=h || (x>=2*h  &&  x <= 3*h))) || (y<=h  && ( x == 0  ||  x == 3*h)) || ((y>=h &&  y<=h*4)  &&  (x==h   ||  x ==h*2)) || ((x>=h &&  x<=2*h)  && y == h*4  ))
+    {
+        return "Border";
+    }
+
+    //Inside
+    else if ((y<=h  &&  x <= 3*h) || ((x>=h  &&  x <= 2*h) && (y>=h &&  y<=h*4) ))
+    {
+        return "Inside";
+    }
+ 
+    //Outside
+    else 
+    {
+        return "Outside";
+    }
+}
+
+
+main()
+{
+    int h,x,y;
+    cout <<"Enter Height: ";
+    cin >> h;
+    cout <<"Enter x coordinate: ";
+    cin >> x;
+    cout <<"Enter y coordinate: ";
+    cin >> y;
+    string point = check_point(h,x,y);
+    cout <<point;
+}
